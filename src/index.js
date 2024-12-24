4f1e7038fea5939629f4e3b8cecc011e6da935e0
TsaqifHasbi <tsaqifhasbi1717@gmail.com>
2024-12-25 04:11:21 +0700

    Update Quote.js to fetch and display quotes correctly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// filepath: /c:/Users/Asus/OneDrive/Documents/KULIAH/freeCodeCamp/FrontEndDeveloper/random-quote-machine/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
