fe2159ff66aa4b43b915f8f13389d44c98225c59
TsaqifHasbi <tsaqifhasbi1717@gmail.com>
2024-12-25 04:07:14 +0700

    Update Quote.js to fetch and display quotes correctly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Quote from './components/Quote';
-import './styles.css';
 
-function App() {
+const App = () => {
     return (
         <div className="App">
             <Quote />
         </div>
     );
-}
+};
 
 export default App;
